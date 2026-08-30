92df8cdb8a9727aba32a4487ff9e0cd2c77a7d91
Ria Tandon <rtandonn@umich.edu>
2026-08-30 17:09:52 -0400

    Setup

---
+++ experiment-vis.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.patches import Patch
---
+++ primary-vis.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.patches import Patch
+
